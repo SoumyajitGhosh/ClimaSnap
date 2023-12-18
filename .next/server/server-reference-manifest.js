32d96def0bc506196a1ba25470c1e870a4551ba1
SOUMYAJIT GHOSH <soumyajitghosh@SOUMYAJITs-MacBook-Pro.local>
2023-12-19 00:52:48 +0530

    style:favicon,description

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"3l1ZL+PvY6p3tKrwNwF+vR1qKouu2mDPALo1mFR7nIE=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"sqfD/rk4lBDFsrX/n8yKSQSnfETWVbe4uth8poGfVm4=\"\n}"
